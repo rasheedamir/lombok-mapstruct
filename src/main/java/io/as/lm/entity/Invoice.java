dc8d26e27230842da482d4a0a57b7ca5bcd156a5
Rasheed Amir <rasheed@aurorasolutions.io>
2017-02-28 16:41:49 +0100

    use builder pattern on constructor

--- src/main/java/io/as/lm/entity/Invoice.java
+++ src/main/java/io/as/lm/entity/Invoice.java
@@ -3,10 +3,20 @@ package io.as.lm.entity;
 import lombok.Builder;
 import lombok.Value;
 
-@Builder(builderClassName = "Builder", builderMethodName = "newBuilder", toBuilder = true)
+//@Builder(builderClassName = "Builder", builderMethodName = "newBuilder", toBuilder = true)
 @Value
 public class Invoice
 {
     String name;
 
+    Invoice()
+    {
+        name = null;
+    }
+
+    @lombok.Builder(builderClassName = "Builder", builderMethodName = "newBuilder", toBuilder = true)
+    private Invoice(String name)
+    {
+        this.name = name;
+    }
 }
